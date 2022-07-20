cfa316f0fa8d0554dbe7481a9a506cf74cb6f43a
4eyalush <4eyalush@gmail.com>
2022-07-20 13:33:00 +0300

    Sliver app bar at Product details

--- shop/lib/screens/product_details.dart
+++ shop/lib/screens/product_details.dart
@@ -19,49 +19,79 @@ class ProductDetailsScreen extends StatelessWidget {
     ).getBy(id: id);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Hero(
-              tag: product.id,
-              child: Image.network(
-                product.imageUrl,
-                height: 400,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                '\$${product.price}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
-                    color: Theme.of(context).colorScheme.secondary),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: SizedBox(
-                width: double.infinity,
-                child: Text(
-                  product.description,
-                  softWrap: true,
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: Theme.of(context).colorScheme.primary),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            leading: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
+              child: Container(
+                color: Colors.black38,
+                child: IconButton(
+                  color: Colors.white,
+                  icon: const Icon(
+                    Icons.chevron_left,
+                    size: 40,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
               ),
             ),
-          ],
-        ),
+            expandedHeight: 500,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  width: double.infinity,
+                  color: Colors.black38,
+                  child: Text(product.title)),
+              background: Hero(
+                tag: product.id,
+                child: Image.network(
+                  product.imageUrl,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    '\$${product.price}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        color: Theme.of(context).colorScheme.secondary),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      product.description,
+                      softWrap: true,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 100,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
