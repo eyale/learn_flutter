8c79067352647d773a047c6e3713543567a81dce
4eyalush <4eyalush@gmail.com>
2022-08-02 16:32:19 +0300

    remove file

--- places/lib/misc/location_helper.dart
+++ places/lib/misc/location_helper.dart
@@ -11,8 +11,6 @@ class LocationHelper {
   }) {
     final apiKey = getApiKey();
 
-    print(
-        'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Alabel:A%7C$latitude,$longitude&key=$apiKey');
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Alabel:A%7C$latitude,$longitude&key=$apiKey';
   }
 }
