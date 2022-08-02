04b871dccb48b7ee4c6f97603fe21bb063966fdf
4eyalush <4eyalush@gmail.com>
2022-08-02 16:37:46 +0300

    remove file

--- places/ios/Runner/AppDelegate.swift
+++ places/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAbI9WHwxkzsa-m1icLkF-QKEHDECYQYhk")
+    GMSServices.provideAPIKey("-")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
